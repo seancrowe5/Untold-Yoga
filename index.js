3bf95c4e6af03755ebdcfa766a28948828163e95
seancrowe5 <sean.crowe@me.com>
2016-10-03 09:38:30 -0500

    public

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ var api = new ParseServer({
 var app = express();
 
 // Serve static assets from the /public folder
-app.use('/public', express.static(path.join(__dirname, '/public')));
+app.use('/', express.static(__dirname + '/public'));
 
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
